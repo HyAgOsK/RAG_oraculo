5b5ee08b361cd1fc257a4ca0e28a1775a6adbc0b
HyAgOsK <hyagovieira@geb.inatel.br>
2025-06-10 23:52:35 -0300

    Deploy

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Set the OpenAI API key
 OPENAI_API_KEY = env('OPENAI_API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', default=True)
+DEBUG = env.bool('DEBUG', default=False)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
